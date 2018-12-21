f499ac83b905c1d7eb1a7249078b8690cb0e844c
joncomo <jon@underground.net>
2018-12-20 19:26:25 -0800

    playing with idea of a network object

--- main.js
+++ main.js
@@ -3909,14 +3909,7 @@ function Text(text, pos) {
 
         let t = this.properties[frame].t;
 
-        // if this is visnet split the network
-        if (t.indexOf('visnet') != -1) {
-            
-            return;
-        }
-
         // for each character, make it it's own text obj
-        
         if (!t) {
             return;
         }
@@ -4827,6 +4820,319 @@ function Text(text, pos) {
     this.parse_text(text);
 }
 
+function Network(pos) {
+    this.type = "Network";
+    this.guid = guid();
+    this.properties = {};
+    this.properties[frame] = {layers: [2,3,2], p: pos, c: [0, 0, 0, 1], w: 1, h: 1, r: 0};
+
+    // ephemeral
+    this.new = true; // loaded or just created
+    this.selected = false;
+    this.dragged = false;
+    
+    this.select = function() {
+        this.selected = true;
+    }
+
+    this.is_selected = function() {
+        return this.selected;
+    }
+
+    this.hidden = function() {
+        if (!this.properties[frame]) {
+            return true;
+        }
+        
+        return this.properties[frame].c[3] == 0;
+    }
+
+    this.copy_properties = function(f, n) {
+        this.properties[n] = copy(this.properties[f]);
+    }
+
+    this.duplicate = function() {
+        if (!this.selected) {
+            return;
+        }
+
+        let newc = new Network(null);
+        newc.properties[frame] = copy(this.properties[frame]);
+        newc.selected = true;
+        this.selected = false;
+        objs.push(newc);
+    }
+
+    this.hide = function() {
+        if (this.selected) {
+            if (this.properties[frame].c[3] == 1) {
+                this.properties[frame].c[3] = 0;
+            } else {
+                this.properties[frame].c[3] = 1;
+            }
+            this.selected = false;
+        }
+    }
+
+    this.set_color = function(rgba) {
+        if (this.selected) {
+            rgba[3] = this.properties[frame].c[3];
+            this.properties[frame].c = rgba;
+        }
+    }
+
+    this.clear_props = function(f) {
+        delete this.properties[f];
+    }
+
+    this.clear_all_props = function() {
+        if (!this.selected) {
+            return;
+        }
+
+        for (var key in this.properties) {
+            if (key != frame) {
+                delete this.properties[key];
+            }
+        }
+    }
+
+    this.del_props_before = function() {
+        if (!this.selected) {
+            return;
+        }
+        
+        if (this.properties && this.properties[frame-1]) {
+            delete this.properties[frame-1];
+        }
+    }
+
+    this.near_mouse = function () {
+        let props = this.properties[frame];
+        if (!props) {
+            return false;
+        }
+
+        return distance(props.p, mouse) < grid_size/2
+    }
+
+    this.in_rect = function(x, y, x2, y2) {
+        if (this.hidden()) {
+            return false;
+        }
+
+        let props = this.properties[frame];
+        let p = props.p;
+
+        if (p.x > x && p.x < x2 && p.y > y && p.y < y2) {
+            this.selected = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    this.mouse_down = function(evt) {
+        if (this.hidden()) {
+            return false;
+        }
+
+        // try to selected one
+        if (this.near_mouse()) {
+            this.selected = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    this.mouse_drag = function(evt) {
+        if (this.selected && tool == "select") {
+            // move
+            let props = this.properties[frame];
+            let offset = {x: mouse_grid.x - mouse_grid_last.x,
+                        y: mouse_grid.y - mouse_grid_last.y};
+            let p = props.p;
+            this.properties[frame].p = {x: p.x + offset.x, y: p.y + offset.y};
+        }
+    }
+
+    this.mouse_up = function(evt) {
+        if (!shift) {
+            this.selected = false;
+        }
+    }
+
+    this.draw_network = function(props, ctx) {
+        let p = props.p;
+        ctx.save();
+        ctx.translate(p.x, p.y);
+        ctx.rotate(props.r);
+        ctx.scale(props.w, props.h);
+
+        layers = props.layers;
+
+        let pad = 120;
+
+        let pos = [0, 0];
+
+        let radius = 14;
+
+        loc = function(i, j, units) {
+            let pad2 = 250;
+            //return [pos[0] - pad2/2 - j*(pad2+80), pos[1] + pad2/2 - pad2 * units/2 + i*pad2];
+            return [pos[0] - pad2 * units/2 + pad2/2 + i*pad2, -pad + pos[1] - j*pad2];
+        }
+
+        // connections
+        let high_conn = [];
+        let high_neur = [];
+
+        for (let j = 0; j < layers.length-1; j++) {
+            let units = layers[j];
+            let units_next = layers[j+1];
+            
+            
+            for (let i = 0; i < units; i++) {
+                let p = loc(i, j, units);
+
+                for (let k = 0; k < units_next; k++) {
+
+                    let p2 = loc(k, j+1, units_next);
+
+                    /*
+                    let vline = [p2[0] - p[0], p2[1] - p[1]];
+                    let mvect = [mouse.x - p[0], mouse.y - p[1]];
+
+                    let dot = mvect[0] * vline[0] + mvect[1] * vline[1];
+
+                    let vlen = math.norm(vline);
+                    let total_len = vlen * math.norm(mvect);
+
+                    if (dot > total_len * .998 && dot < vlen*vlen) {
+                        ctx.strokeStyle = "red";
+                    } else {
+                        ctx.strokeStyle = "black";
+                    } */
+
+                    ctx.strokeStyle = "black";
+
+                    if (high_conn.length == 0) {
+                        let dx1 = p[0] - mouse.x;
+                        let dy1 = p[1] - mouse.y;
+
+                        let dx2 = p2[0] - mouse.x;
+                        let dy2 = p2[1] - mouse.y;
+
+                        let d1 = math.sqrt(dx1*dx1 + dy1*dy1);
+                        let d2 = math.sqrt(dx2*dx2 + dy2*dy2);
+
+                        let vline = [p2[0] - p[0], p2[1] - p[1]];
+                        let vlen = math.norm(vline);
+
+                        if (d1 + d2 < vlen + 1) {
+                            ctx.strokeStyle = "green";
+                            high_conn = [i, k, j]; // unit i to unit k in layer j
+                            high_neur = [[i, j], [k, j+1]];
+                        }
+                    }
+                    
+                    ctx.beginPath();
+                    ctx.moveTo(p[0], p[1]);
+                    ctx.lineTo(p2[0], p2[1]);
+                    ctx.stroke();
+                }
+            }
+        }
+
+        ctx.fillStyle = 'white';
+
+        // neurons
+        for (let j = 0; j < layers.length; j++) {
+            let units = layers[j];
+
+            for (let i = 0; i < units; i++) {
+                let p = loc(i, j, units);
+
+                ctx.strokeStyle = "black";
+
+                // if we have a highlighted connection and we're in the right layer
+                if (high_conn.length != 0) {
+
+                    if (high_conn[2] == j) {
+                        if (high_conn[0] == i) {
+                            if (j == 0) {
+                                ctx.strokeStyle = "blue";
+                            } else {
+                                ctx.strokeStyle = "red";
+                            }
+                        }
+                    } else if (high_conn[2] == j-1) {
+                        if (high_conn[1] == i) {
+                            if (j == 0) {
+                                ctx.strokeStyle = "blue";
+                            } else {
+                                ctx.strokeStyle = "red";
+                            }
+                        }
+                    }
+
+                    
+
+                } else {
+                    let dx = mouse.x - p[0];
+                    let dy = mouse.y - p[1];
+                    
+                    if (dx*dx + dy*dy < 400) {
+                        if (j == 0) {
+                            ctx.strokeStyle = "blue";
+                        } else {
+                            ctx.strokeStyle = "red";
+                        }
+                        
+                        high_neur = [[i, j]];
+                    }
+                }
+
+                ctx.beginPath();
+                ctx.arc(p[0], p[1], radius, 0, 2*Math.PI);
+                ctx.fill();
+                ctx.stroke();
+            }
+        }
+
+        ctx.restore();
+    }
+
+    this.render = function(ctx) {
+
+        let a = this.properties[frame];
+        let b = this.properties[next_frame];
+
+        if (!a) {
+            return;
+        }
+
+        let props;
+        if (transition.transitioning) {
+            props = interpolate(a, b);
+        } else {
+            props = a;
+        }
+
+
+        ctx.beginPath();
+        this.draw_network(props, ctx);
+
+        if (!presenting && props.c[3] != 0 && (this.selected || this.near_mouse())) {
+            ctx.beginPath();
+            ctx.strokeStyle = dark;
+            ctx.strokeRect(props.p.x - grid_size/4, props.p.y - grid_size/4, grid_size/2, grid_size/2);
+            ctx.stroke();
+        }
+    }
+}
+
 function Camera() {
     this.default_props = {p: {x:c.width/2, y:c.height/2}, w: 1, h: 1, rxyz: [0, 0, 0], style: "3d"};
     this.properties = {};
@@ -5369,6 +5675,11 @@ function Menu(pos) {
     this.buttons.push(new Button("vector", {x: 0, y: 0}, function(b) {
         tool = "vector";
     }));
+
+    /*
+    this.buttons.push(new Button("network", {x: 0, y: 0}, function(b) {
+        tool = "network";
+    })); */
 
     this.buttons.push(new Button("delete", {x: 0, y: 0}, function(b) {
         let N = objs.length;
@@ -6452,6 +6763,9 @@ window.onload = function() {
         } else if (tool == "circle") {
             let new_circle = new Circle([0, 0, 0, 1], mouse_grid);
             objs.push(new_circle);
+        } else if (tool == "network") {
+            let n = new Network(mouse_grid);
+            objs.push(n);
         }
 
         if (selecting) {
