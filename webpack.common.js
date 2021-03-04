8d0ddce5dddc5e636df4ca729dfce611025a07d8
catruge <49883288+catruge@users.noreply.github.com>
2021-03-04 14:57:07 -0500

    Configure contenthash and HtmlWebpackPlugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: {
+    main: path.resolve(__dirname, 'src', 'index.js'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'template.html'),
+    }),
+  ],
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
