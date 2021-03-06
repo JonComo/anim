64cc3fb3fb5bdcdb3a2faea1b33c0fd243ca011a
catruge <49883288+catruge@users.noreply.github.com>
2021-03-06 14:49:48 -0500

    Move 'Animator' class to src/animator.js

--- src/index.js
+++ src/index.js
@@ -2503,41 +2503,6 @@ function guid() {
     s4() + '-' + s4() + s4() + s4();
 }
 
-function Animator(fps, canvas, frames, callback) {
-    this.fps = fps;
-    this.canvas = canvas;
-    this.frames = frames;
-    this.callback = callback;
-
-    if (this.frames > 0) {
-        // Create a capturer that exports a WebM video
-        this.capturer = new CCapture( { format: 'png', framerate: this.fps } );
-        this.capturer.start();
-    }
-
-    this.animate = function () {
-        if (this.frames > 0) {
-            this.frames -= 1;
-            requestAnimationFrame(this.animate);
-        } else {
-            if (this.capturer) {
-                this.capturer.stop();
-                this.capturer.save();
-                this.capturer = null;
-            }
-
-            setTimeout(function() {
-                requestAnimationFrame(this.animate);
-            }, 1000/this.fps);
-        }
-
-        this.callback();
-        this.capturer.capture(this.canvas);
-    }
-
-    this.animate();
-}
-
 function pretty_round(num) {
     return (Math.round(num*100)/100).toFixed(2);
 }
