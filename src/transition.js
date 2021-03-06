9ed6e3e7df236a73776bca4ecc62f5d9e8c18b18
catruge <49883288+catruge@users.noreply.github.com>
2021-03-06 18:56:59 -0500

    separate-transition

---
+++ src/transition.js
@@ -0,0 +1,43 @@
+import { ease_in_out, sigmoid } from './index';
+import { math, parser, rtv } from './resources';
+
+export default function Transition() {
+  this.steps = 0;
+  this.step = 0;
+  this.transitioning = false;
+  this.target_frame = 0;
+
+  this.run = (steps, targetFrame, completion) => {
+    if (this.transitioning) {
+      return;
+    }
+
+    rtv.t_percent = 0.0;
+    rtv.t_ease = 0.0;
+    rtv.t_in_out = 1.0;
+    this.steps = steps;
+    this.target_frame = targetFrame;
+    this.transitioning = true;
+    this.completion = completion;
+  };
+
+  this.update = () => {
+    if (this.transitioning) {
+      this.step += 1;
+      rtv.t_percent = this.step / this.steps;
+      rtv.t_in_out = -math.cos(rtv.t_percent * 2 * math.PI - math.PI) / 2 + 0.5;
+      parser.set('_t', rtv.t_percent);
+      rtv.t_ease = ease_in_out(rtv.t_percent);
+      parser.set('_tt', rtv.t_ease);
+      rtv.t_ease = sigmoid(rtv.t_percent, 1.2, -0.4, 14) - sigmoid(rtv.t_percent, 0.2, -0.6, 15);
+      if (this.step >= this.steps) {
+        rtv.t_percent = 1.0;
+        rtv.t_in_out = 1.0;
+        rtv.t_ease = 1.0;
+        this.completion(this.target_frame);
+        this.step = 0;
+        this.transitioning = false;
+      }
+    }
+  };
+}
