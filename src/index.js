691507340eb6c3bfab4bec913351cbd8e9a0e15e
catruge <e7rtzb5c1@relay.firefox.com>
2021-03-06 19:52:45 -0500

    separate-menu

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { saveAs } from 'file-saver';
 import $ from 'jquery';
-import Button from './button';
 import Circle from './circle';
 import Frames from './frames';
+import Menu from './menu';
 import Pen from './pen';
 import Shape from './shape';
 import Transition from './transition';
@@ -21,7 +21,6 @@ import {
   BRACKETS,
   PI2,
   MAT_NUM_WIDTH,
-  VERSION,
   CHAR,
 } from './resources';
 
@@ -3143,7 +3142,7 @@ export function rgbToHex(c) {
     return "#" + ((1 << 24) + (Math.round(c[0]) << 16) + (Math.round(c[1]) << 8) + Math.round(c[2])).toString(16).slice(1);
 }
 
-function hexToRgb(hex) {
+export function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? [
         parseInt(result[1], 16),
@@ -5016,11 +5015,11 @@ function load(evt) {
     reader.readAsText(f);
 }
 
-function save_local() {
+export function save_local() {
     localStorage.setItem('page', state_to_string());
 }
 
-function load_local() {
+export function load_local() {
     // Grab the objects from storage
     let page = localStorage.getItem('page');
     if (page && page.length) {
@@ -5079,235 +5078,13 @@ export function insert_frame() {
     rtv.frames.create_buttons();
 }
 
-function present() {
+export function present() {
     rtv.tool = "select";
     rtv.presenting = true;
     document.body.style.cursor = 'none';
     document.body.scrollTop = 0; // Scroll to top in Safari
     document.documentElement.scrollTop = 0; // Scroll to top in other browsers
     document.body.style.overflow = 'hidden'; // Disable and hide scrollbar
-}
-
-function Menu(pos) {
-    this.pos = pos;
-    this.buttons = [];
-
-    this.buttons.push(new Button("select", {x: 0, y: 0}, function(b) {
-        enter_select();
-    }));
-
-    this.buttons.push(new Button("text", {x: 0, y: 0}, function(b) {
-        rtv.tool = "text";
-    }));
-
-    this.buttons.push(new Button("pen", {x: 0, y: 0}, function(b) {
-        if (rtv.tool != "pen") {
-            rtv.tool = "pen";
-        } else {
-            rtv.pen.clear_drawing();
-        }
-    }));
-
-    this.buttons.push(new Button("split", {x: 0, y: 0}, function(b) {
-        let N = rtv.objs.length;
-        for (let i = 0; i < N; i++) {
-            let obj = rtv.objs[i];
-            if (typeof obj.split == "function") {
-                obj.split();
-            }
-        }
-    }));
-
-    this.buttons.push(new Button("shape", {x: 0, y: 0}, function(b) {
-        rtv.tool = "shape";
-    }));
-
-    this.buttons.push(new Button("circle", {x: 0, y: 0}, function(b) {
-        rtv.tool = "circle";
-    }));
-
-    this.buttons.push(new Button("vector", {x: 0, y: 0}, function(b) {
-        rtv.tool = "vector";
-    }));
-
-    /*
-    this.buttons.push(new Button("network", {x: 0, y: 0}, function(b) {
-        tool = "network";
-    })); */
-
-    this.buttons.push(new Button("delete", {x: 0, y: 0}, function(b) {
-        let N = rtv.objs.length;
-        for (let i = 0; i < N; i++) {
-            if (rtv.objs[i].is_selected()) {
-                rtv.objs[i].deleted = true;
-            }
-        }
-    }));
-
-    this.buttons.push(new Button("del props all", {x: 0, y: 0}, function(b) {
-        let N = rtv.objs.length;
-        for (let i = 0; i < N; i++) {
-            let obj = rtv.objs[i];
-            if (typeof obj.clear_all_props == "function") {
-                obj.clear_all_props();
-            }
-        }
-    }));
-
-    this.buttons.push(new Button("del props before", {x: 0, y: 0}, function(b) {
-        let N = rtv.objs.length;
-        for (let i = 0; i < N; i++) {
-            let obj = rtv.objs[i];
-            if (typeof obj.del_props_before == "function") {
-                obj.del_props_before();
-            }
-        }
-    }));
-
-    this.buttons.push(new Button("duplicate", {x: 0, y: 0}, function(b) {
-        for (let i = rtv.objs.length-1; i >= 0; i--) {
-            let obj = rtv.objs[i];
-            if (typeof obj.duplicate == "function") {
-                obj.duplicate();
-            }
-        }
-    }));
-
-    this.buttons.push(new Button("copy frame", {x: 0, y: 0}, function(b) {
-        rtv.tool = "copy frame";
-    }));
-
-    this.buttons.push(new Button("hide", {x: 0, y: 0}, function(b) {
-        let N = rtv.objs.length;
-        for (let i = 0; i < N; i++) {
-            let obj = rtv.objs[i];
-            if (typeof obj.hide == "function") {
-                obj.hide();
-            }
-        }
-    }));
-
-    this.buttons.push(new Button("pres. hide", {x: 0, y: 0}, function(b) {
-        let N = rtv.objs.length;
-        for (let i = 0; i < N; i++) {
-            let obj = rtv.objs[i];
-            if (obj.properties && obj.is_selected()) {
-                obj.properties[rtv.frame]['ph'] = true;
-            }
-        }
-    }));
-
-    this.buttons.push(new Button("pres. show", {x: 0, y: 0}, function(b) {
-        let N = rtv.objs.length;
-        for (let i = 0; i < N; i++) {
-            let obj = rtv.objs[i];
-            if (obj.properties && obj.is_selected()) {
-                obj.properties[rtv.frame]['ph'] = false;
-            }
-        }
-    }));
-
-    this.buttons.push(new Button("camera", {x: 0, y: 0}, function(b) {
-        if (rtv.tool == "camera") {
-            // reset the camera rotation
-            rtv.cam.properties[rtv.frame].rxyz = [0,0,0];
-            rtv.cam.properties[rtv.frame].p = rtv.cam.default_props.p;
-        }
-        rtv.tool = "camera";
-    }));
-
-    this.buttons.push(new Button("csys", {x: 0, y: 0}, function(b) {
-        let csys_style = rtv.cam.style();
-
-        if (csys_style == "3d") {
-            rtv.cam.set_style("flat");
-            rtv.cam.properties[rtv.frame].w = 1.5;
-            rtv.cam.properties[rtv.frame].h = 1.5;
-            rtv.cam.rotate([-Math.PI/2,0,-Math.PI/2]);
-        } else if (csys_style == "flat") {
-            rtv.cam.set_style("none");
-        } else if (csys_style == "none") {
-            rtv.cam.set_style("3d");
-            rtv.cam.properties[rtv.frame].w = 1;
-            rtv.cam.properties[rtv.frame].h = 1;
-        }
-    }));
-
-    this.buttons.push(new Button("view xy", {x: 0, y: 0}, function(b) {
-        rtv.cam.rotate([-Math.PI/2,0,-Math.PI/2]);
-    }));
-
-    this.buttons.push(new Button("frame", {x: 0, y:0}, function(b){
-        rtv.view_frame = !rtv.view_frame;
-    }));
-
-    this.buttons.push(new Button("debug", {x: 0, y: 0}, function(b) {
-        rtv.debug = !rtv.debug;
-    }));
-
-    this.buttons.push(new Button("present", {x: 0, y: 0}, function(b) {
-        // show a cursor
-        present();
-    }));
-
-    this.buttons.push(new Button("save local", {x: 0, y: 0}, function(b) {
-        // Put the object into storage
-        save_local();
-    }));
-
-    this.buttons.push(new Button("load local", {x: 0, y: 0}, function(b) {
-        load_local();
-    }));
-
-    this.buttons.push(new Button(`ver: ${VERSION}`, {x: 0, y: 0}, function(b) {
-
-    }));
-
-    for (let i = 0; i < COLORS.length; i++) {
-
-        let b = new Button("", {x: 0, y: 0}, function(b) {
-            let rgb = hexToRgb(COLORS[i]);
-
-            rtv.pen.set_color(rgb);
-
-            for (let i = 0; i < rtv.objs.length; i++) {
-                let obj = rtv.objs[i];
-                if (typeof obj.set_color === "function") {
-                    obj.set_color(rgb);
-                }
-            }
-        });
-        b.color = COLORS[i];
-        this.buttons.push(b);
-    }
-
-    for (let i = 0; i < this.buttons.length; i++) {
-        let b = this.buttons[i];
-        b.pos = {x: this.pos.x, y: this.pos.y + i * GRID_SIZE*.6};
-    }
-
-    this.mouse_up = function(evt) {
-        for (let i = 0; i < this.buttons.length; i++) {
-            let btn = this.buttons[i];
-            if (btn.mouse_up(evt)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    this.render = function(ctx) {
-        ctx.fillStyle = "#000000";
-        for (let i = 0; i < this.buttons.length; i++) {
-            let b = this.buttons[i];
-            b.selected = false;
-            if (b.text == rtv.tool) {
-                b.selected = true;
-            }
-            b.render(ctx);
-        }
-    };
 }
 
 function constrain_frame(f) {
@@ -5508,7 +5285,7 @@ export function transition_with_next(next) {
     });
 }
 
-function enter_select() {
+export function enter_select() {
     rtv.tool = "select";
     rtv.new_line = null;
 }
