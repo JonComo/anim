0d4008bfd30d963e5477ae473be00abaaca846e9
catruge <49883288+catruge@users.noreply.github.com>
2021-03-11 09:34:13 -0500

    Refactor and format

--- src/audio/volume-meter.js
+++ src/audio/volume-meter.js
@@ -38,25 +38,89 @@ clipLag: how long you would like the "clipping" indicator to show
 Access the clipping through node.checkClipping(); use node.shutdown to get rid of it.
 */
 
-let audioContext;
-let mediaStreamSource;
 let meter;
 export default meter;
 
+function volumeAudioProcess(event) {
+  const buf = event.inputBuffer.getChannelData(0);
+  const bufLength = buf.length;
+  let sum = 0;
+  let x;
+
+  // Do a root-mean-square on the samples: sum up the squares...
+  for (let i = 0; i < bufLength; i++) {
+    x = buf[i];
+    if (Math.abs(x) >= this.clipLevel) {
+      this.clipping = true;
+      this.lastClip = window.performance.now();
+    }
+    sum += x * x;
+  }
+
+  // ... then take the square root of the sum.
+  const rms = Math.sqrt(sum / bufLength);
+
+  // Now smooth this out with the averaging factor applied
+  // to the previous sample - take the max here because we
+  // want "fast attack, slow release."
+  this.volume = Math.max(rms, this.volume * this.averaging);
+}
+
+function createAudioMeter(audioContext, clipLevel, averaging, clipLag) {
+  const processor = audioContext.createScriptProcessor(512);
+  processor.onaudioprocess = volumeAudioProcess;
+  processor.clipping = false;
+  processor.lastClip = 0;
+  processor.volume = 0;
+  processor.clipLevel = clipLevel || 0.98;
+  processor.averaging = averaging || 0.95;
+  processor.clipLag = clipLag || 750;
+
+  // this will have no effect, since we don't copy the input to the output,
+  // but works around a current Chrome bug.
+  processor.connect(audioContext.destination);
+
+  processor.checkClipping = () => {
+    if (!this.clipping) return false;
+    if ((this.lastClip + this.clipLag) < window.performance.now()) this.clipping = false;
+    return this.clipping;
+  };
+
+  processor.shutdown = () => {
+    this.disconnect();
+    this.onaudioprocess = null;
+  };
+
+  return processor;
+}
+
 export function initVolumeMeter() {
+  let mediaStreamSource;
+
   // volume meter
   // monkeypatch Web Audio
-  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  window.AudioContext ??= window.webkitAudioContext;
 
   // grab an audio context
-  audioContext = new AudioContext();
+  const audioContext = new AudioContext();
+
+  function gotStream(stream) {
+    // Create an AudioNode from the stream.
+    mediaStreamSource = audioContext.createMediaStreamSource(stream);
+
+    // Create a new volume meter and connect it.
+    meter = createAudioMeter(audioContext);
+    mediaStreamSource.connect(meter);
+  }
+
+  function didntGetStream() {
+    alert('Stream generation failed.');
+  }
 
   // Attempt to get audio input
   try {
     // monkeypatch getUserMedia
-    navigator.getUserMedia = navigator.getUserMedia
-            || navigator.webkitGetUserMedia
-            || navigator.mozGetUserMedia;
+    navigator.getUserMedia ??= navigator.webkitGetUserMedia ?? navigator.mozGetUserMedia;
 
     // ask for an audio input
     navigator.getUserMedia(
@@ -76,71 +140,5 @@ export function initVolumeMeter() {
     alert(`getUserMedia threw exception :${e}`);
   }
 
-  function didntGetStream() {
-    alert('Stream generation failed.');
-  }
-
   mediaStreamSource = null;
-
-  function gotStream(stream) {
-    // Create an AudioNode from the stream.
-    mediaStreamSource = audioContext.createMediaStreamSource(stream);
-
-    // Create a new volume meter and connect it.
-    meter = createAudioMeter(audioContext);
-    mediaStreamSource.connect(meter);
-  }
-}
-
-function createAudioMeter(audioContext, clipLevel, averaging, clipLag) {
-  const processor = audioContext.createScriptProcessor(512);
-  processor.onaudioprocess = volumeAudioProcess;
-  processor.clipping = false;
-  processor.lastClip = 0;
-  processor.volume = 0;
-  processor.clipLevel = clipLevel || 0.98;
-  processor.averaging = averaging || 0.95;
-  processor.clipLag = clipLag || 750;
-
-  // this will have no effect, since we don't copy the input to the output,
-  // but works around a current Chrome bug.
-  processor.connect(audioContext.destination);
-
-  processor.checkClipping =		function () {
-		  if (!this.clipping) return false;
-		  if ((this.lastClip + this.clipLag) < window.performance.now()) this.clipping = false;
-		  return this.clipping;
-  };
-
-  processor.shutdown =		function () {
-		  this.disconnect();
-		  this.onaudioprocess = null;
-  };
-
-  return processor;
-}
-
-function volumeAudioProcess(event) {
-  const buf = event.inputBuffer.getChannelData(0);
-  const bufLength = buf.length;
-  let sum = 0;
-  let x;
-
-  // Do a root-mean-square on the samples: sum up the squares...
-  for (let i = 0; i < bufLength; i++) {
-    	x = buf[i];
-    	if (Math.abs(x) >= this.clipLevel) {
-    		this.clipping = true;
-    		this.lastClip = window.performance.now();
-    	}
-    	sum += x * x;
-  }
-
-  // ... then take the square root of the sum.
-  const rms = Math.sqrt(sum / bufLength);
-
-  // Now smooth this out with the averaging factor applied
-  // to the previous sample - take the max here because we
-  // want "fast attack, slow release."
-  this.volume = Math.max(rms, this.volume * this.averaging);
 }
