ffdaf6715ec8bfed2a806ece2811fb54b85af3d5
catruge <e7rtzb5c1@relay.firefox.com>
2021-03-11 08:56:28 -0500

    Fix errors and format

--- src/audio/volume-meter.js
+++ src/audio/volume-meter.js
@@ -38,116 +38,109 @@ clipLag: how long you would like the "clipping" indicator to show
 Access the clipping through node.checkClipping(); use node.shutdown to get rid of it.
 */
 
-var audioContext;
-var mediaStreamSource;
-var meter;
-var meterInitialized = false;
+let audioContext;
+let mediaStreamSource;
+let meter;
+export default meter;
 
-function initVolumeMeter() {
-	// volume meter
-    // monkeypatch Web Audio
-	window.AudioContext = window.AudioContext || window.webkitAudioContext;
+export function initVolumeMeter() {
+  // volume meter
+  // monkeypatch Web Audio
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-	// grab an audio context
-	audioContext = new AudioContext();
+  // grab an audio context
+  audioContext = new AudioContext();
 
-    // Attempt to get audio input
-    try {
-        // monkeypatch getUserMedia
-        navigator.getUserMedia = 
-            navigator.getUserMedia ||
-            navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia;
+  // Attempt to get audio input
+  try {
+    // monkeypatch getUserMedia
+    navigator.getUserMedia = navigator.getUserMedia
+            || navigator.webkitGetUserMedia
+            || navigator.mozGetUserMedia;
 
-        // ask for an audio input
-        navigator.getUserMedia(
-        {
-            "audio": {
-                "mandatory": {
-                    "googEchoCancellation": "false",
-                    "googAutoGainControl": "false",
-                    "googNoiseSuppression": "false",
-                    "googHighpassFilter": "false"
-                },
-                "optional": []
-            },
-        }, gotStream, didntGetStream);
+    // ask for an audio input
+    navigator.getUserMedia(
+      {
+        audio: {
+          mandatory: {
+            googEchoCancellation: 'false',
+            googAutoGainControl: 'false',
+            googNoiseSuppression: 'false',
+            googHighpassFilter: 'false',
+          },
+          optional: [],
+        },
+      }, gotStream, didntGetStream,
+    );
+  } catch (e) {
+    alert(`getUserMedia threw exception :${e}`);
+  }
 
-    } catch (e) {
-        alert('getUserMedia threw exception :' + e);
-    }
+  function didntGetStream() {
+    alert('Stream generation failed.');
+  }
 
-    function didntGetStream() {
-        alert('Stream generation failed.');
-    }
+  mediaStreamSource = null;
 
-    mediaStreamSource = null;
+  function gotStream(stream) {
+    // Create an AudioNode from the stream.
+    mediaStreamSource = audioContext.createMediaStreamSource(stream);
 
-    function gotStream(stream) {
-        // Create an AudioNode from the stream.
-        mediaStreamSource = audioContext.createMediaStreamSource(stream);
-
-        // Create a new volume meter and connect it.
-        meter = createAudioMeter(audioContext);
-        mediaStreamSource.connect(meter);
-    }
+    // Create a new volume meter and connect it.
+    meter = createAudioMeter(audioContext);
+    mediaStreamSource.connect(meter);
+  }
 }
 
-function createAudioMeter(audioContext,clipLevel,averaging,clipLag) {
-	var processor = audioContext.createScriptProcessor(512);
-	processor.onaudioprocess = volumeAudioProcess;
-	processor.clipping = false;
-	processor.lastClip = 0;
-	processor.volume = 0;
-	processor.clipLevel = clipLevel || 0.98;
-	processor.averaging = averaging || 0.95;
-	processor.clipLag = clipLag || 750;
+function createAudioMeter(audioContext, clipLevel, averaging, clipLag) {
+  const processor = audioContext.createScriptProcessor(512);
+  processor.onaudioprocess = volumeAudioProcess;
+  processor.clipping = false;
+  processor.lastClip = 0;
+  processor.volume = 0;
+  processor.clipLevel = clipLevel || 0.98;
+  processor.averaging = averaging || 0.95;
+  processor.clipLag = clipLag || 750;
 
-	// this will have no effect, since we don't copy the input to the output,
-	// but works around a current Chrome bug.
-	processor.connect(audioContext.destination);
+  // this will have no effect, since we don't copy the input to the output,
+  // but works around a current Chrome bug.
+  processor.connect(audioContext.destination);
 
-	processor.checkClipping =
-		function(){
-			if (!this.clipping)
-				return false;
-			if ((this.lastClip + this.clipLag) < window.performance.now())
-				this.clipping = false;
-			return this.clipping;
-		};
+  processor.checkClipping =		function () {
+		  if (!this.clipping) return false;
+		  if ((this.lastClip + this.clipLag) < window.performance.now()) this.clipping = false;
+		  return this.clipping;
+  };
 
-	processor.shutdown =
-		function(){
-			this.disconnect();
-			this.onaudioprocess = null;
-		};
+  processor.shutdown =		function () {
+		  this.disconnect();
+		  this.onaudioprocess = null;
+  };
 
-	return processor;
+  return processor;
 }
 
-function volumeAudioProcess( event ) {
-	var buf = event.inputBuffer.getChannelData(0);
-    var bufLength = buf.length;
-	var sum = 0;
-    var x;
+function volumeAudioProcess(event) {
+  const buf = event.inputBuffer.getChannelData(0);
+  const bufLength = buf.length;
+  let sum = 0;
+  let x;
 
-	// Do a root-mean-square on the samples: sum up the squares...
-    for (var i=0; i<bufLength; i++) {
+  // Do a root-mean-square on the samples: sum up the squares...
+  for (let i = 0; i < bufLength; i++) {
     	x = buf[i];
-    	if (Math.abs(x)>=this.clipLevel) {
+    	if (Math.abs(x) >= this.clipLevel) {
     		this.clipping = true;
     		this.lastClip = window.performance.now();
     	}
     	sum += x * x;
-    }
+  }
 
-    // ... then take the square root of the sum.
-    var rms =  Math.sqrt(sum / bufLength);
+  // ... then take the square root of the sum.
+  const rms = Math.sqrt(sum / bufLength);
 
-    // Now smooth this out with the averaging factor applied
-    // to the previous sample - take the max here because we
-    // want "fast attack, slow release."
-    this.volume = Math.max(rms, this.volume*this.averaging);
+  // Now smooth this out with the averaging factor applied
+  // to the previous sample - take the max here because we
+  // want "fast attack, slow release."
+  this.volume = Math.max(rms, this.volume * this.averaging);
 }
-
-
