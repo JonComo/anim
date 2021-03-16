1ae808f6a0da9bda7101a2d971c425dc689622ac
catruge <e7rtzb5c1@relay.firefox.com>
2021-03-16 12:58:19 -0400

    math.draw

--- src/index.js
+++ src/index.js
@@ -650,13 +650,13 @@ math.import({
   },
   draw(points, fill) { // draws line from point to point [[x1,y1,z1], ...], draws arrow
     const N = points.size()[0];
-    points = rtv.cam.graph_to_screen_mat(points);
+    const pointsL = rtv.cam.graph_to_screen_mat(points);
 
     rtv.ctx.save();
     rtv.ctx.beginPath();
     let p; let lastp;
     for (let i = 0; i < N; i++) {
-      p = points[i];
+      p = pointsL[i];
       if (i === 0) {
         rtv.ctx.moveTo(p[0], p[1]);
       } else {
