5a21875a1cf8570bf9d5825770a4298bde64ad38
ESLint Formatter <runner@fv-az204-264.hhaqkv3es3sufp1bmcp1cup05e.cx.internal.cloudapp.net>
2021-03-16 01:22:09 +0000

    Format

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
