eee3e6e0fec1d745479c909046f42068529116fa
catruge <e7rtzb5c1@relay.firefox.com>
2021-03-17 20:34:42 -0400

    Remove debugging code

--- src/index.js
+++ src/index.js
@@ -3696,7 +3696,6 @@ window.onload = () => {
 
   document.addEventListener('paste', (event) => {
     const paste = (event.clipboardData || window.clipboardData).getData('text');
-    console.log(`pasting: ${paste}`);
 
     const N = rtv.objs.length;
     for (let i = 0; i < rtv.objs.length; i++) {
