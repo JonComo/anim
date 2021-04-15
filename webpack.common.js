20966d4c5bf759daa5486151639e791878e495ff
catruge <e7rtzb5c1@relay.firefox.com>
2021-04-15 18:06:41 -0400

    Update path to include src/assets/

--- webpack.common.js
+++ webpack.common.js
@@ -27,7 +27,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'template.html'),
-      favicon: path.resolve(__dirname, 'src', 'favicon.ico'),
+      favicon: path.resolve(__dirname, 'src', 'assets', 'favicon.ico'),
     }),
   ],
   output: {
